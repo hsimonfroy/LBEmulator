052772082303dccb8c90363c6eb85fcca9309017
Martin White <mwhite@berkeley.edu>
2019-10-07 17:12:35 -0700

    Setting up to do galaxy files as well.

--- code/robust_avg.py
+++ code/robust_avg.py
@@ -82,6 +82,41 @@ def average_halo_spectra(lgMmin,lgMmax,zz):
         ff.write("{:15.5e} {:15.5e} {:15.5e} {:15.5e} {:15.5e}\n".\
                  format(kk[i],mu_hh[i],sig_hh[i],mu_hm[i],sig_hm[i]))
     ff.close()
+    #
+
+
+
+
+
+def average_galaxy_spectra(zz):
+    """Does the robust average of the galaxy spectra."""
+    # First read the data, throwing away the first row (which is k=0).
+    iz = int(100*zz+0.01)
+    fn = db+"pg_z{:03d}_????.txt".format(iz)
+    kk = read_column(fn,0)[0,1:]
+    phh= read_column(fn,1)[:,1:]
+    phm= read_column(fn,2)[:,1:]
+    # Now we compute robust averages, which aren't really necessary
+    # for the galaxies which are quite clean.
+    mu_hh,sig_hh = robust_avg(phh)
+    mu_hm,sig_hm = robust_avg(phm)
+    # and write the summary file.
+    fout=db+"pg_z{:03d}.txt".format(iz)
+    ff  = open(fout,"w")
+    ff.write("# Galaxy (HOD) auto- and cross-power spectra.\n")
+    ff.write("# Robust average of {:d} spectra using sqrt(1+x^2).\n".\
+             format(phh.shape[0]))
+    ff.write("# {:05.2f}<lgM<{:05.2f}, z={:.3f}\n".format(lgMmin,lgMmax,zz))
+    ff.write("# {:>13s} {:>15s} {:>15s} {:>15s} {:>15s}\n".\
+             format("k[h/Mpc]","<Phh>","Err[Phh]","<Phm>","Err[Phm]"))
+    for i in range(kk.size):
+        ff.write("{:15.5e} {:15.5e} {:15.5e} {:15.5e} {:15.5e}\n".\
+                 format(kk[i],mu_hh[i],sig_hh[i],mu_hm[i],sig_hm[i]))
+    ff.close()
+    #
+
+
+
 
 
 
@@ -91,4 +126,7 @@ if __name__=="__main__":
     for i in range(len(lgMmin)):
         for zz in [0.0,1.0]:
             average_halo_spectra(lgMmin[i],lgMmax[i],zz)
+    #
+    #for zz in [1.0]:
+    #    average_galaxy_spectra(zz)
     #
