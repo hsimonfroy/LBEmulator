67593a3a9c9869fcc42777ccfdf07865b2985451
Martin White <mwhite@berkeley.edu>
2019-10-08 15:16:15 -0700

    Overwrite N-body with PT numbers at low k.

--- code/assemble_files.py
+++ code/assemble_files.py
@@ -17,6 +17,7 @@ destdir = "/global/cscratch1/sd/mwhite/LagEmu/AllSpectra"
 dirlist = ["/global/cscratch1/sd/mwhite/LagEmu/N2048-T40-B2/",
            "/global/cscratch1/sd/sfschen/lagrangian_emulator"+\
            "/data/N2048-T40-B2/"]
+
 # Seeds to look for.
 slist   = range(9200,9210)
 
@@ -52,17 +53,46 @@ def copy_galaxy_spectra():
 
 
 def copy_component_spectra():
-    """Copies the component spectrum files."""
+    """Copies the component spectrum files -- some of the columns are
+    very noisy at low k, so we use PT to overwrite those."""
     for idir in dirlist:
         for iseed in slist:
             db = idir + "S{:04d}/spectra".format(iseed)
             if os.path.isdir(db):
                 for infn in glob.glob(db+"/spectra-z???-R0.txt"):
+                    pk    = np.loadtxt(infn)
+                    # Overwrite the "noisy" columns with PT.
+                    zz    = float(infn[len(db)+10:len(db)+13])/100.
+                    ptfn  = "../theory/"
+                    ptfn += "cleft_components_z_{:3.1f}.dat".format(zz)
+                    clpt  = np.loadtxt(ptfn)
+                    # Replace ( 1,b2), column 3 with column 6 in PT file.
+                    ww       = np.nonzero( pk[:,0]<0.10 )[0]
+                    pk[ww,3] = np.interp(pk[ww,0],clpt[:,0],clpt[:, 6])
+                    # Replace (b1,b2), column 7 with column 7 in PT file.
+                    ww       = np.nonzero( pk[:,0]<0.15 )[0]
+                    pk[ww,7] = np.interp(pk[ww,0],clpt[:,0],clpt[:, 7])
+                    # Replace ( 1,bs), column 4 with column 9 in PT file.
+                    ww       = np.nonzero( pk[:,0]<0.15 )[0]
+                    pk[ww,4] = np.interp(pk[ww,0],clpt[:,0],clpt[:, 9]/2)
+                    # Replace (b1,bs), column 4 with column 9 in PT file.
+                    ww       = np.nonzero( pk[:,0]<0.15 )[0]
+                    pk[ww,8] = np.interp(pk[ww,0],clpt[:,0],clpt[:,10]/2)
+                    # Now write the modified data to file.
                     outfn = infn.rstrip(".txt")[len(db)+1:]
                     outfn+= "_{:04d}.txt".format(iseed)
                     outfn = outfn.replace("-","_")
                     outfn = outfn.replace("spectra","pc")
-                    shutil.copy2(infn,destdir+"/"+outfn)
+                    #
+                    fout  = open(destdir+"/"+outfn,"w")
+                    fout.write("# Component spectra for z={:f}.\n".format(zz))
+                    for i in range(pk.shape[0]):
+                        outstr = "{:15.5e}".format(pk[i,0])
+                        for j in range(1,pk.shape[1]):
+                            outstr += " {:15.5e}".format(pk[i,j])
+                        fout.write(outstr+"\n")
+                    fout.close()
+                    #shutil.copy2(infn,destdir+"/"+outfn)
 
 
 
